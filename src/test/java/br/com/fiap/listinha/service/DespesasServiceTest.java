445a2f749b4c7e1c7d30e6d4a4566aeb2cc35cd8
Guihayashi <guilherme-hayashi@hotmail.com>
2022-11-02 23:50:20 -0300

    Primeiro teste unitário da função de criar uma despesa e verificar se foi criada com a categoria correta.

---
+++ src/test/java/br/com/fiap/listinha/service/DespesasServiceTest.java
@@ -0,0 +1,59 @@
+package br.com.fiap.listinha.service;
+
+import br.com.fiap.listinha.dto.NovaDespesaDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+class DespesasServiceTest {
+
+    @Test
+    void listarDespesas() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void buscarDespesaPorId() {
+    }
+
+    @Test
+    void criar() {
+        //Setup
+        DespesasService despesasService = Mockito.mock(DespesasServiceImpl.class);
+
+        NovaDespesaDTO novaDespesaDTO = new NovaDespesaDTO();
+
+        //Mock
+        NovaDespesaDTO mockDespesa = new NovaDespesaDTO();
+        mockDespesa.setCategoria("Conta de água");
+        mockDespesa.setStatus("Pago");
+        mockDespesa.setValor(new BigDecimal("76.57"));
+        mockDespesa.setDataVencimento(new Date(04/11/2022));
+        mockDespesa.setDescricao("Conta de água referente ao mês de Outubro");
+        Mockito.when(despesasService.criar(novaDespesaDTO)).thenReturn(mockDespesa);
+
+        //Call
+        novaDespesaDTO = (NovaDespesaDTO) despesasService.criar(novaDespesaDTO);
+
+        //Assertions
+        Assertions.assertEquals("Conta de água",novaDespesaDTO.getCategoria());
+    }
+
+    @Test
+    void atualizar() {
+    }
+
+    @Test
+    void atualizarPreco() {
+    }
+
+    @Test
+    void deletarDespesa() {
+    }
+}
